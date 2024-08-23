088435eb30f11b3e3010ad74f0f01c031c854c74
Denis Kukobin <denis.kukobin@hipasus.com>
2024-08-23 22:10:17 +0300

    added frame

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,18 @@ const App = () => {
         <HeroSection />
         <FeatureSection />
         <Workflow />
+        <div id="dexscreener-embed" style={{ marginTop: "60px" }}>
+          <h2 className="text-3xl sm:text-5xl lg:text-6xl text-center mt-40 tracking-wide mb-10">
+            Token{" "}
+            <span className="bg-gradient-to-r from-orange-500 to-orange-800 text-transparent bg-clip-text">
+              Graph
+            </span>
+          </h2>
+          <iframe
+            src="https://dexscreener.com/ton/EQCaY8Ifl2S6lRBMBJeY35LIuMXPc8JfItWG4tl7lBGrSoR2?embed=1&theme=dark"
+            style={{ width: "100%", height: "700px", border: "none" }}
+          ></iframe>
+        </div>
         <Footer />
       </div>
     </>
